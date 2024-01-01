0a959ec0e64ab357de7131f7c9ae9a9125f46e92
Max Miliano <milianordelcanto@gmail.com>
2024-01-01 20:46:44 -0300

    refactor: lualine

--- lua/lualine/themes/hydra.lua
+++ lua/lualine/themes/hydra.lua
@@ -1,33 +1,31 @@
 local colors = require('hydra.colors')
-local color = require('hydra.color')
-local base05 = color.darken(colors.base04, 10)
 
 local hydra = {}
 
 hydra.normal = {
   a = { bg = colors.green, fg = colors.base03 },
-  b = { bg = colors.base04, fg = colors.green },
-  c = { bg = base05, fg = colors.base0 },
+  b = { bg = colors.base02, fg = colors.green },
+  c = { bg = colors.base04, fg = colors.base0 },
 }
 
 hydra.insert = {
   a = { bg = colors.blue, fg = colors.base03 },
-  b = { bg = colors.base04, fg = colors.blue },
+  b = { bg = colors.base02, fg = colors.blue },
 }
 
 hydra.command = {
   a = { bg = colors.purple, fg = colors.base03 },
-  b = { bg = colors.base04, fg = colors.purple },
+  b = { bg = colors.base02, fg = colors.purple },
 }
 
 hydra.visual = {
   a = { bg = colors.yellow, fg = colors.base03 },
-  b = { bg = colors.base04, fg = colors.yellow },
+  b = { bg = colors.base02, fg = colors.yellow },
 }
 
 hydra.replace = {
   a = { bg = colors.red, fg = colors.base03 },
-  b = { bg = colors.base04, fg = colors.red },
+  b = { bg = colors.base02, fg = colors.red },
 }
 
 return hydra
