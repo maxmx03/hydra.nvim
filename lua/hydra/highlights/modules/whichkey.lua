d17d4d4f3bac283ecc2fc059b387026e5e7baf5c
Max Miliano <milianordelcanto@gmail.com>
2024-01-02 09:42:37 -0300

    refactor: wk

--- lua/hydra/highlights/modules/whichkey.lua
+++ lua/hydra/highlights/modules/whichkey.lua
@@ -7,7 +7,7 @@ M.load = function(opts)
   local c = opts.colors
 
   hl('WhichKey', { fg = c.teal }) -- the key
-  hl('WhichKeyGroup', { fg = c.pink }) -- a group
+  hl('WhichKeyGroup', { fg = c.yellow }) -- a group
   hl('WhichKeySeparator', { fg = c.red }) -- the separator between the key and its label
   hl('WhichKeyDesc', { fg = c.base0 }) -- the label of the key
   -- hl('WhichKeyFloat') -- Normal in the popup window
