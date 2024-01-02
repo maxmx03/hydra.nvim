12d82c1f5d27634e0815799bc350da04a502aec3
Max Miliano <milianordelcanto@gmail.com>
2024-01-02 14:25:48 -0300

    refactor: format code

--- sample/run.sh
+++ sample/run.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	realpath() { [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"; }
-	ROOT=$(dirname $(dirname $(realpath "$0")))
+    realpath() { [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"; }
+    ROOT=$(dirname $(dirname $(realpath "$0")))
 else
-	ROOT=$(dirname $(dirname $(readlink -f $0)))
+    ROOT=$(dirname $(dirname $(readlink -f $0)))
 fi
 
 DEVELOPER=$(xcode-select -print-path)
 LIPO=$(xcrun -sdk iphoneos -find lipo)
 
 function code() {
-	cd $ROOT
+    cd $ROOT
 
-	# Node modules
-	test -d node_modules || ./scripts/npm.sh install
+    # Node modules
+    test -d node_modules || ./scripts/npm.sh install
 
-	# Configuration
-	export NODE_ENV=development
+    # Configuration
+    export NODE_ENV=development
 
-	# Launch Code
-	if [[ "$OSTYPE" == "darwin"* ]]; then
-		exec ./.build/electron/Electron.app/Contents/MacOS/Electron . "$@"
-	else
-		exec ./.build/electron/electron . "$@"
-	fi
+    # Launch Code
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        exec ./.build/electron/Electron.app/Contents/MacOS/Electron . "$@"
+    else
+        exec ./.build/electron/electron . "$@"
+    fi
 }
 
 code "$@"
